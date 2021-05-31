b2589e7a7fa2ce26b2e600dbfc8d8f2f0cfb998c
1569690947@qq.com <1569690947@qq.com>
2021-05-31 20:03:08 +0800

    qzjwzx

--- Clothes_UI/Controllers/HomeController.cs
+++ Clothes_UI/Controllers/HomeController.cs
@@ -8,25 +8,8 @@ namespace Clothes_UI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       public ActionResult Home()
         {
-            //hhfhfh,iimiikikikikiikii
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            //yhyhyh
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            //gyhgyhgyhyh
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
